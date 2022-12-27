0ce59e985a6c0a02554c44e394d01bcd5dd6c956
ahmed-farrag <ahmedfaarrag@gmail.com>
2022-12-27 22:48:24 +0200

    second one

---
+++ Bank_Management_Sys.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+// #include<conio.h>
+
+using namespace std;
+
+class bank{
+    public: 
+      void open_account();
+      void deposit_money();
+      void withdrow_money();
+      void displau_account();
+};
+
+void bank::open_account(){
+    cout<<"\nEnter your Name"<<endl;
+    cout<<"\n"<<endl;
+    cout<<"\n"<<endl;
+    cout<<"\n"<<endl;
+    
+}
